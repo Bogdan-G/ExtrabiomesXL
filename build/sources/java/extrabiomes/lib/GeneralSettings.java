9af4deb1ddf962e25e990bce4049c520c7f41004
Bogdan <bogdangtt@gmail.com>
2016-04-14 00:13:11 +0300

    fix

---
+++ build/sources/java/extrabiomes/lib/GeneralSettings.java
@@ -0,0 +1,11 @@
+package extrabiomes.lib;
+
+public class GeneralSettings
+{
+    
+    public static boolean bigTreeSaplingDropModifier = false;
+    public static boolean consoleCommandsDisabled    = true;
+    public static boolean useLegacyRedwoods = false;
+    public static boolean useMC18Doors = true;
+    
+}
